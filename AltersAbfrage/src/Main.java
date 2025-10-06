8ce3a0eea82a822253a3f7cba3e94246004f14c8
TerezaazereT <the.terezareck@gmail.com>
2025-10-06 11:08:26 +0200

    Add files via upload
    Version_1.0

---
+++ AltersAbfrage/src/Main.java
@@ -0,0 +1,46 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bist du 18 Jahre alt? ");
+        String eingabe = scanner.nextLine();
+
+
+
+        if (eingabe.equalsIgnoreCase("Ja")||eingabe.equalsIgnoreCase("Yes")) {
+            System.out.println("Okdoky");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+        /*System.out.print("Bist du 18 Jahre alt?");
+
+        String Input;
+        Scanner eingabewert = new Scanner(System.in);
+        Input = eingabewert.nextLine();
+
+        if (eingabewert.equalsIgnoreCase("Ja"));
+        System.out.println("OK");
+
+
+
+        }
+
+
+
+                //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+    }*/
